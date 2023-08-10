b090642115857dd54102aa203826cd28c47abb46
avindutt <avindutt2369@gmail.com>
2023-08-10 12:13:00 +0530

    create emoji input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import './App.css';
+import Picker from '@emoji-mart/react'
+import data from '@emoji-mart/data';
 
 function App() {
   const [message, setMessage] = useState('');
   const [messageList, setMessageList] = useState([]);
+  const [showEmoji, setShowEmoji] = useState(false);
 
   const user_list = ["Alan", "Bob", "Carol", "Dean", "Elin"];
 
@@ -17,6 +20,9 @@ function App() {
   };
 
   const handleSend = () => {
+    if(message === ''){
+      return;
+    }
     setMessageList([...messageList, newMessage]);
     setMessage('');
   }
@@ -34,6 +40,11 @@ function App() {
     setMessageList(updatedMessages);
   }
 
+  const handleEmojiClick = (emoji) => {
+    setMessage(message + emoji.native);
+    setShowEmoji(false);
+  }
+
   return (
     <div className="main-container">
       <h1>Let's have a Chat!</h1>
@@ -46,7 +57,7 @@ function App() {
               <p>{item.inputs}</p>
             </div>
             <div>
-              <span>{item.like}</span>
+              <span className='like-count'>{item.like}</span>
               <button onClick={() => handleLikeCount(index)}><img src="https://cdn-icons-png.flaticon.com/256/889/889140.png" alt="like-button" /></button>
             </div>
           </div>
@@ -54,7 +65,19 @@ function App() {
       </div>
       <div className='chat-input'>
         <input type="text" onKeyUp={handleKeyPress} placeholder='Type your message...' onChange={(e)=>setMessage(e.target.value)} value={message} />
-        <button onClick={handleSend}><img src="https://cdn-icons-png.flaticon.com/256/3682/3682321.png" alt="send" /></button>
+        <div className='emotes'>
+          <button onClick={()=>setShowEmoji(!showEmoji)} className='emote'><span>&#x1F604;</span></button>
+          <button onClick={handleSend}><img src="https://cdn-icons-png.flaticon.com/256/3682/3682321.png" alt="send" /></button>
+        </div>
+        {showEmoji && (
+          <div className="emoji-dir">
+          <Picker
+          data={data}
+          previewPosition="none"
+          onEmojiSelect={(e)=>handleEmojiClick(e)}
+          />
+          </div>
+        )}
       </div>
     </div>
   );
